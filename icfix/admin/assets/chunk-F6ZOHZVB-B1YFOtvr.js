071f50004c62e57339c900b563e87c492a072599
khoinguyent <khoinguyent@gmail.com>
2025-12-12 20:41:54 +0700

    Build and commit admin static files for Vercel pre-built deployment

---
+++ icfix/admin/assets/chunk-F6ZOHZVB-B1YFOtvr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DI3nk9N_.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
---
+++ icfix/admin/assets/chunk-RQYKGJZ4--RGyTRAn.js
@@ -0,0 +1,1 @@
+import{aD as i}from"./index-DI3nk9N_.js";var a=i("product","*categories,*shipping_profile,-variants");export{a as P};
